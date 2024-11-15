fe3c147eb771fd33afc14880a3293bd079c49a80
Gloire-Owen <159003104+Gloire-Owen@users.noreply.github.com>
2024-11-16 02:46:13 +0530

    Add files via upload

---
+++ Html/Html/rjs/Index.jsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='flex-row-ac'>
+        <div className='ellipse' />
+        <div className='logo' />
+        <span className='acceuil'>Acceuil</span>
+        <span className='services'>Services </span>
+        <span className='offres'>Offres</span>
+        <span className='contacts'>Contacts </span>
+        <div className='meuf-du-debut' />
+        <span className='image-de-marque'>
+          Créer vous une image de marque
+          <br />
+        </span>
+        <div className='line' />
+        <span className='determine-problem'>
+          Allow us to determine the problem, find the objectives the range of
+          the study, to find the unit on which we will base our study and we
+          will perform. Determine also the limits of our system
+        </span>
+        <div className='rectangle'>
+          <span className='contacter'>CONTACTER</span>
+        </div>
+      </div>
+      <span className='nos-services'>Nos services</span>
+      <div className='flex-row'>
+        <div className='vector' />
+        <div className='vector-1' />
+        <div className='boost' />
+      </div>
+      <div className='flex-row-a'>
+        <div className='regroup'>
+          <span className='boost-image'>Boost d’image </span>
+          <span className='web-site'>Web site</span>
+        </div>
+        <span className='image-de-marque-2'>Image de marque</span>
+      </div>
+      <div className='navigator' />
+      <div className='flex-row-a-3'>
+        <div className='photos' />
+        <span className='creation-image-marque'>
+          Création d'une image de marque
+        </span>
+        <span className='determine-problem-objectives'>
+          Allow us to determine the problem, find the objectives the range of
+          the study, to find the unit on which we will base our study and we
+          will perform. Determine also the limits of our system
+        </span>
+        <div className='ellipse-4' />
+        <div className='rectangle-5'>
+          <span className='contacter-6'>CONTACTER</span>
+        </div>
+        <span className='nos-offres'>Nos offres</span>
+        <div className='rectangle-7'>
+          <span className='brand-booster'>Brand booster </span>
+          <div className='flex-row-8'>
+            <span className='dollar'>30 $</span>
+            <span className='pour-un-mois'>Pour un mois </span>
+          </div>
+          <div className='flex-row-e'>
+            <div className='vector-9' />
+            <span className='design-logo'>Designe d’un logo</span>
+          </div>
+          <div className='flex-row-b'>
+            <div className='vector-a' />
+            <span className='create-posters'>Creation d’affiches </span>
+          </div>
+          <div className='flex-row-ef'>
+            <div className='vector-b' />
+            <span className='design-business-cards'>
+              Designe de cartes visite
+            </span>
+          </div>
+          <div className='flex-row-c'>
+            <div className='vector-d' />
+            <span className='boost-visibility-social'>
+              Boost de visibilité sur les reseaux
+            </span>
+          </div>
+          <div className='rectangle-e'>
+            <span className='contact'>CONTACTER</span>
+          </div>
+        </div>
+        <div className='rectangle-f'>
+          <span className='web-master'>Web master </span>
+          <div className='flex-row-10'>
+            <span className='price-250'>250 $</span>
+            <span className='per-month'>Pour un mois </span>
+          </div>
+          <div className='flex-row-c-11'>
+            <div className='vector-12' />
+            <span className='design-logo-13'>Designe d’un logo</span>
+          </div>
+          <div className='flex-row-e-14'>
+            <div className='vector-15' />
+            <span className='creation-d-affiches'>Creation d’affiches </span>
+          </div>
+          <div className='flex-row-ce'>
+            <div className='vector-16' />
+            <span className='designe-de-cartes-visite'>
+              Designe de cartes visite
+            </span>
+          </div>
+          <div className='flex-row-17'>
+            <div className='vector-18' />
+            <span className='boost-de-visibilite-sur-les-reseaux'>
+              Boost de visibilité sur les reseaux
+            </span>
+          </div>
+          <div className='flex-row-cf'>
+            <div className='vector-19' />
+            <span className='creation-d-un-site-web'>
+              Création d’un site web
+            </span>
+          </div>
+          <div className='rectangle-1a'>
+            <span className='contacter-1b'>CONTACTER</span>
+          </div>
+        </div>
+      </div>
+      <div className='rectangle-1c' />
+      <div className='rectangle-1d' />
+      <div className='flex-row-ad'>
+        <span className='contacts-1e'>CONTACTS</span>
+        <span className='liens-utiles'>LIENS UTILES</span>
+        <div className='line-1f' />
+      </div>
+      <div className='flex-row-20'>
+        <div className='vector-21' />
+        <span className='plus'>+243 995 929 491</span>
+        <span className='creation-de-logo'>Creation de logo</span>
+        <span className='creation-d-affiche'>Creation d’affiche</span>
+      </div>
+      <div className='flex-row-bf'>
+        <div className='vector-22' />
+        <span className='lifticon'>lifticon</span>
+        <span className='marketing-digital'>Marketing digital</span>
+        <span className='image-de-marque-23'>Image de marque</span>
+      </div>
+      <div className='flex-row-fd'>
+        <div className='vector-24' />
+        <span className='lifticon-gmail-com'>lifticon@gmail.com</span>
+        <span className='visibilite'>Visiblité</span>
+      </div>
+      <div className='flex-row-f'>
+        <div className='vector-25' />
+        <span className='lifticon-26'>lifticon</span>
+        <span className='creation-de-site-web'>Creation de site web</span>
+      </div>
+    </div>
+  );
+}
